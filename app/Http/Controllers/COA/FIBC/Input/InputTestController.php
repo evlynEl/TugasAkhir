f56ec0a6fd1414476effdf5f5483fc2121ede23a
evlynEl <evelyneleonora38@gmail.com>
2024-07-22 15:53:19 +0700

    feat<COA>
    button referensi selesai

--- app/Http/Controllers/COA/FIBC/Input/InputTestController.php
+++ app/Http/Controllers/COA/FIBC/Input/InputTestController.php
@@ -27,10 +27,10 @@ class InputTestController extends Controller
         //
     }
 
-    public function show($id)
+    public function show($id, Request $request)
     {
         if ($id == 'getRef') {
-            $refNo = DB::connection('ConnTestQC')->select('exec [SP_1273_QTC_MAINT_FIBC] @kode = ?', [9]);
+            $refNo = DB::connection('ConnTestQC')->select('exec [SP_1273_QTC_MAINT_FIBC] @Kode = ?', [9]);
             $data_Ref = [];
             foreach ($refNo as $detailRef) {
                 $data_Ref[] = [
@@ -39,7 +39,48 @@ class InputTestController extends Controller
                 ];
             }
             return datatables($data_Ref)->make(true);
+        } else if ($id == 'getDetailCyclic') {
+            $cyclic = DB::connection('ConnTestQC')->select('exec [SP_1273_QTC_MAINT_FIBC] @Kode = ?, @RefNo = ?', [10, $request->input('no_ref')]);
+
+            if (!empty($cyclic)) {
+                $cyclicData = $cyclic[0];
+                $swl = $cyclicData->SWL;
+                $cyclicTestValue = 2 * $swl;
+                $refCopy = $cyclicData->Copy_RefNo;
+
+                $data_additional = [];
+                if (!empty($refCopy)) {
+                    $additionalData = DB::connection('ConnTestQC')->select('exec [SP_1273_QTC_LIST_FIBC] @kode = ?, @RefNo = ?', [3, $refCopy]);
+
+                    foreach ($additionalData as $data_detailAdd) {
+                        $data_additional[] = [
+                            'cyclicCheckbox' => $data_detailAdd->Cyclic_Lift,
+                            'cyclicResult' => $data_detailAdd->Cyclic_Result,
+                            'topLiftCheckbox' => $data_detailAdd->Top_Lift,
+                            'dropCheckbox' => $data_detailAdd->Drop_Result,
+                            'heightApprox' => $data_detailAdd->Height_Approx,
+                            'diaVal' => $data_detailAdd->Dia,
+                            'squareVal' => $data_detailAdd->Square,
+                            'loadSpeed' => $data_detailAdd->Load_Speed,
+                            'dropTest' => $data_detailAdd->Drop_Test,
+                        ];
+                    }
+
+                    return response()->json([
+                        'cyclicTestValue' => $cyclicTestValue,
+                        'additionalData' => $data_additional,
+                        'RefCopy' => $refCopy
+                    ]);
+                }
+            }
+
+            return response()->json([
+                'cyclicTestValue' => null,
+                'additionalData' => [],
+                'RefCopy' => null
+            ]);
         }
+
     }
 
     public function edit($id)
